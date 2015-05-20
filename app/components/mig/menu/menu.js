3548b9e79051862aefc41420ec6cbe1c4ba72cfd
Sebastiaan Deckers <seb@ninja.sg>
2015-05-20 12:40:44 +0800

    Separate CSS file

--- app/components/mig/menu/menu.js
+++ app/components/mig/menu/menu.js
@@ -1,16 +1,12 @@
 /*global HTMLElement */
 import html from './menu.jade'
+import css from './menu.css'
 
 class Menu extends HTMLElement {
   createdCallback () {
     let root = this.createShadowRoot()
     root.innerHTML = html()
-    this.style.display = 'inline-block'
-    this.style.position = 'fixed'
-    this.style.top = '0px'
-    this.style.bottom = '20px'
-    this.style.right = '0px'
-    this.style.width = '300px'
+    css.insert(root)
   }
 }
 
