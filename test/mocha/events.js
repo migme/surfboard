f140abbbd02ddf04811a29c20c709671b5119a5b
Sebastiaan Deckers <seb@ninja.sg>
2015-06-23 23:32:57 +0800

    Reduced duplication

--- test/mocha/events.js
+++ test/mocha/events.js
@@ -2,22 +2,18 @@
 import {dispatch, bubble} from '../../app/utils/events'
 
 describe('events wrapper', () => {
+  const target = { dispatchEvent: function () {} }
+  sinon.stub(target, 'dispatchEvent')
+  const type = 'foo'
+  const payload = { bar: 123 }
   it('dispatches', () => {
-    const target = { dispatchEvent: function () {} }
-    sinon.stub(target, 'dispatchEvent')
-    const type = 'foo'
-    const payload = { bar: 123 }
     target::dispatch(type, payload)
-    var expected = { type, detail: payload }
-    expect(target.dispatchEvent).to.have.been.calledWithMatch(expected)
+    expect(target.dispatchEvent)
+      .to.have.been.calledWithMatch({ type, detail: payload })
   })
   it('bubbles', () => {
-    const target = { dispatchEvent: function () {} }
-    sinon.stub(target, 'dispatchEvent')
-    const type = 'foo'
-    const payload = { bar: 123 }
     target::bubble(type, payload)
-    var expected = { bubbles: true }
-    expect(target.dispatchEvent).to.have.been.calledWithMatch(expected)
+    expect(target.dispatchEvent)
+      .to.have.been.calledWithMatch({ bubbles: true })
   })
 })
