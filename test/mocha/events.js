7057e553574c7092fb100dbe39c05285eb1aecde
Sebastiaan Deckers <seb@ninja.sg>
2015-06-23 23:28:20 +0800

    Testing dispatch and bubble

--- test/mocha/events.js
+++ test/mocha/events.js
@@ -1,12 +1,23 @@
-/* global describe it */
+/* global describe it expect sinon */
+import {dispatch, bubble} from '../../app/utils/events'
 
-import events from '../../app/utils/events'
-import {expect} from 'chai'
-
-describe('events', () => {
-  it('dispatch', () => {
-    const target = sinon.stub({}, 'dispatchEvent')
-    target::dispatch('foo', { bar: 123 })
-    expect(target).to.have.been.called
+describe('events wrapper', () => {
+  it('dispatches', () => {
+    const target = { dispatchEvent: function () {} }
+    sinon.stub(target, 'dispatchEvent')
+    const type = 'foo'
+    const payload = { bar: 123 }
+    target::dispatch(type, payload)
+    var expected = { type, detail: payload }
+    expect(target.dispatchEvent).to.have.been.calledWithMatch(expected)
+  })
+  it('bubbles', () => {
+    const target = { dispatchEvent: function () {} }
+    sinon.stub(target, 'dispatchEvent')
+    const type = 'foo'
+    const payload = { bar: 123 }
+    target::bubble(type, payload)
+    var expected = { bubbles: true }
+    expect(target.dispatchEvent).to.have.been.calledWithMatch(expected)
   })
 })
