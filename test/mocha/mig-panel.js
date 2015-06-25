079dd445044c6041280b98a133696974a14c977a
Sebastiaan Deckers <seb@ninja.sg>
2015-06-25 16:42:57 +0800

    Tests for mig-panel

--- test/mocha/mig-panel.js
+++ test/mocha/mig-panel.js
@@ -8,17 +8,22 @@ describe('panel', () => {
     widget = document.querySelector('mig-me')
   })
   it('is hidden by default', () => {
-    let panel = widget.shadowRoot.querySelector('mig-panel')
+    const panel = widget.shadowRoot.querySelector('mig-panel')
     expect(panel).to.not.exist
   })
   it('is visible when opened', () => {
     widget::dispatch('toggle')
-    let panel = widget.shadowRoot.querySelector('mig-panel')
+    const panel = widget.shadowRoot.querySelector('mig-panel')
     expect(panel).to.exist
     const rect = panel.getBoundingClientRect()
     expect(rect.width).to.be.above(0)
     expect(rect.height).to.be.above(0)
     expect(panel.querySelector('mig-menu')).to.exist
-    widget::dispatch('toggle')
+  })
+  it('goes away when pressing the Close button', () => {
+    let panel = widget.shadowRoot.querySelector('mig-panel')
+    panel.shadowRoot.querySelector('button').click()
+    panel = widget.shadowRoot.querySelector('mig-panel')
+    expect(panel).to.not.exist
   })
 })
